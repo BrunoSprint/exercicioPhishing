ca522de60df12edb797b79165a21605e868cebd3
BrunoSprint <117302910+BrunoSprint@users.noreply.github.com>
2023-12-16 00:24:15 -0300

    .

---
+++ logica.js
@@ -0,0 +1,12 @@
+console.log("Player Name")
+
+
+//declara uma variavél
+let nickname = "Config New Name"
+
+
+//concatenando uma  mensagem fixa + uma variável
+console.log("Bem Vindo" + nickname)
+console.log(nickname + "Entrou no Servidor")
+//concatenação de saida fixa
+
---
+++ logica2.js
@@ -0,0 +1,12 @@
+// declaração de constante, use em caso de repetições não variaveis, valor repetitivo
+// as variaveis quando o valor mudar, valor que sofre alterações
+const Notificação = "Muito bem Dev"
+
+
+// saida
+console.log(Notificação + "Qual o seu Nome? Meu nome é Dev.")
+console.log(Notificação + "Muito bem Dev, vamos aprender Java!")
+
+
+
+
